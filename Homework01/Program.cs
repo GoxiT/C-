abca8a18762c5c8c0c7ebf6a25b28f265674006d
Goran Todorovski <goran.todorovski91@gmail.com>
2019-03-18 13:09:15 +0100

    Homework01

---
+++ Homework01/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Excercise 01
+            //Declare a new array of integers with 5 elements
+            //Initialize the array elements with values from input
+            //Sum all the values and print the result in the console
+
+            //int sum = 0;
+            //int[] arr = new int[5];
+            //Console.WriteLine("Enter five numbers: ");
+            //for(var i = 0; i<arr.Length; i++)
+            //{
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //foreach (var item in arr)
+            //{
+            //    sum += item;
+
+            //}
+            //Console.WriteLine("The sum is: "+ sum);
+            //Console.ReadLine();
+
+            // 2.
+            //Create an array with names
+            //Give an option to the user to enter a name from the keyboard
+            //After entering the name put it in the array and print all the names
+            //Ask the user if they want to enter another name(Y / N)
+            //Do the same process over and over until the user enters N
+
+
+            //string[] names = new string[0];
+            //string answer;
+
+            //do
+            //{
+            //    Console.WriteLine("Please enter name:");
+            //    Array.Resize(ref names, names.Length + 1);
+            //    names[names.Length - 1] = Console.ReadLine();
+            //    Console.WriteLine("Do you want to enter another name? (Y / N)");
+            //    answer = Console.ReadLine();
+
+            //} while (answer == "Y");
+            //Console.WriteLine("The names are:");
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+            //Console.ReadLine();
+
+            //Task 2
+            //1.
+            //Create an array of integers with length of 20 and fill the array from user's input.
+
+
+            //int[] arr = new int[20];
+            //for (var i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine("Please enter number: ");
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("The numbers are : ");
+            
+            
+            // Task 3 
+            //Print all numbers of the array, 
+            //in case a number is 0 it should print "Skipped" 
+            //and in case a number is a 3-digit number it should stop the printing.
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if(arr[i]== 0)
+            //    {
+            //        Console.WriteLine("skipped");
+            //    }
+            //    else if (arr[i] > 99 && arr[i] < 1000)
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(arr[i]);
+            //    }
+                
+            //}
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
